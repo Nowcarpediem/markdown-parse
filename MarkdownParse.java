991bb0de48702c504bf656f9ce68a7d108300ab2
Nowcarpediem <dchu@ucsd.edu>
2022-01-27 14:44:35 -0800

    Update MarkdownParse.java

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -16,13 +16,14 @@ public class MarkdownParse {
 
             int nextCloseBracket = markdown.indexOf("]", nextOpenBracket);
             //System.out.println("index of closed bracket: " + nextCloseBracket);
-
+            if (!(markdown.substring(nextOpenBracket, markdown.length()).contains("("))) {
+                break;
+            }
             int openParen = markdown.indexOf("(", nextCloseBracket);
             //System.out.println("index of open paren: " + openParen);
 
             int closeParen = markdown.indexOf(")", openParen);
             //System.out.println("index of closed paren: " + closeParen);
-
             toReturn.add(markdown.substring(openParen + 1, closeParen));
             currentIndex = closeParen + 1;
             
