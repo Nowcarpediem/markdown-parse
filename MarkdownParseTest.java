08c4e008cd27abf3ed956e80fdf86ae981733e94
Nowcarpediem <dchu@ucsd.edu>
2022-02-25 08:33:14 -0800

    1

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -16,15 +16,30 @@ public class MarkdownParseTest<T> {
     }
 
     @Test
-    public void testMethod()  throws IOException { 
+    public void testMethod() throws IOException   { 
         ArrayList<String> toReturn = new ArrayList<>();
         toReturn.add("https://something.com");
         toReturn.add("some-page.html");
+        //Path fileName = Path.of("C:/Users/dylan/Documents/GitHub/markdown-parse/my-test.md");
         Path fileName = Path.of("test-file.md");
 	    String contents = Files.readString(fileName);
         assertEquals(toReturn, MarkdownParse.getLinks(contents));
     }
+    /*
+    @Test
+    public void testMethod2() throws IOException   { 
+        ArrayList<String> toReturn = new ArrayList<>();
+        toReturn.add("https://something.com");
+        toReturn.add("some-page.html");
+        Path fileName = Path.of("C:/Users/dylan/Documents/GitHub/markdown-parse/my-test2.md");
+	    String contents = Files.readString(fileName);
+        assertEquals(toReturn, MarkdownParse.getLinks(contents));
+    }
+    //C:\Users\dylan\Documents\GitHub\markdown-parse\my-test.md
+    //C:\Users\dylan\Documents\GitHub\markdown-parse\my-test2.md
+    C:\Users\dylan\Documents\GitHub\markdown-parse\my-test.md
 
+    */
 }
 
 
